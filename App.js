974c2c1f9c42285671ad264bf1a4e5c7e0a67a07
BiggestCipher <tanmaypilankar@gmail.com>
2021-10-07 20:50:54 +0530

    docs: Updated files.

--- App.js
+++ App.js
@@ -17,13 +17,19 @@ function SubmitHandler() {
 function CalculatePAndL(a, q, c) {
     if (a>0 && q>0 && c>0) {
         if (a>c) {
-            var Loss =((a-c)*q);
+            var Loss=(a-c);
+            var TotalLoss =((a-c)*q);
+            var RoundedTLoss = TotalLoss.toFixed(2);
             var LossPercentage =((Loss/a)*100);
-            Output.innerText=("Sorry! You have suffered a Loss of "+Loss+ " and Loss Percent is "+LossPercentage+"%.")
+            var RoundedLPer = LossPercentage.toFixed(2);
+            Output.innerText=("Sorry! You have suffered a Loss of Rs "+RoundedTLoss+ " and Loss Percent is "+RoundedLPer+"%.")
         } else if (c>a) {
-            var Profit =((c-a)*q);
+            var Profit=(c-a);
+            var TotalProfit =((c-a)*q);
+            var RoundedTProfit = TotalProfit.toFixed(2);
             var ProfitPercentage =((Profit/a)*100); 
-            Output.innerText=("Congrats! You have earned a Profit of "+Profit+" and Profit Percent is "+ProfitPercentage+"%.")
+            var RoundedPPer = ProfitPercentage.toFixed(2);
+            Output.innerText=("Congrats! You have earned a Profit of Rs "+RoundedTProfit+" and Profit Percent is "+RoundedPPer+"%.")
         } else {
             Output.innerText=("Average Price and Current Price are same. So, No Profit and No Loss till now.")
         }
